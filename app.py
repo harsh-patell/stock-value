a08a3206119404f773ffec199f31a439fa4141ce
Harsh Patel <its.hp3@gmail.com>
2022-08-20 14:15:24 -0400

    updated model + graphs

--- app.py
+++ app.py
@@ -1,103 +1,124 @@
 
-import numpy as numpy
+import math
+import requests
+import numpy as np
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 import pandas_datareader as data
 from datetime import datetime
+from bs4 import BeautifulSoup
 from keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
 # import keras.models as kr
 # import sklearn.preprocessing as skl
 
+st.title('Stock Trend Predictor')
 
-# Get the stock information (scraping from Yahoo Finance)
+#####################################
+#        STOCK INFORMATION          #
+#####################################
 start = '2016-01-01'
 end = datetime.today().strftime('%Y-%m-%d')
-
-st.title('Stock Trend Predictor')
-input_ticker = st.text_input('Enter Stock Ticker', 'TSLA')
-
+input_ticker = st.text_input('Enter Stock Ticker', 'AAPL')
 
 df = data.DataReader(input_ticker, 'yahoo', start, end)
-scaler = MinMaxScaler(feature_range=(0,1))
 
+scaler = MinMaxScaler(feature_range=(0,1))
+closingData = df.filter(['Close'])
+dataset = closingData.values
 
 # Describe Raw Data
 st.subheader('Stock Data from 2016 - 2022')
 st.write(df.describe())
 
-
-# Visualizations 
+#####################################
+#           VISUALIZATIONS          #
+##################################### 
 st.subheader('Closing Price vs Time Chart')
-fig = plt.figure(figsize = (12,6))
-plt.xlabel('Time')
-plt.ylabel('Price (USD)')
+fig = plt.figure(figsize = (16,8))
+plt.xlabel('Time', fontsize=12)
+plt.ylabel('Price (USD)', fontsize=12)
 plt.plot(df['Close'], 'b')
 st.pyplot(fig)
 
 st.subheader('Closing Price vs Time Chart with 100MA')
 ma100 = df.Close.rolling(100).mean()
-plt.xlabel('Time')
-plt.ylabel('Price (USD)')
-plt.plot(ma100, 'g')
-plt.plot(df.Close, 'b')
+plt.xlabel('Time', fontsize=12)
+plt.ylabel('Price (USD)', fontsize=12)
+plt.plot(df.Close, 'b', label="Original Price")
+plt.plot(ma100, 'g', label="100 MA")
+plt.legend(loc='best')
 st.pyplot(fig)
 
 st.subheader('Closing Price vs Time Chart with 100MA & 200MA')
 ma200 = df.Close.rolling(200).mean()
 plt.xlabel('Time')
 plt.ylabel('Price (USD)')
-plt.plot(ma100, 'g')
-plt.plot(ma200, 'r')
 plt.plot(df['Close'], 'b')
+plt.plot(ma100, 'g')
+plt.plot(ma200, 'r', label="200 MA")
+plt.legend(loc='best')
 st.pyplot(fig)
 
 
 # Splitting Data into Training and Testing
-data_training = pd.DataFrame(df['Close'][0:int(len(df)*0.70)]) 
-data_testing = pd.DataFrame(df['Close'][int(len(df)*0.70): int(len(df))])
+training_data_len = math.ceil(int(len(df) * 0.8))
+data_training = pd.DataFrame(df['Close'][0 : training_data_len]) 
+data_testing = pd.DataFrame(df['Close'][training_data_len: int(len(df))])
 
 print(data_training.shape) # Number of rows to train data on
 print(data_testing.shape)  # Number of rows to test data on
 
-
 # Loading the LSTM Model (training our data)
-model = load_model('keras_model.h5')
+model = load_model('keras_new_model.h5')
 
-# Scaling the testing data after appending it to last 100 days of training data
-past_100_days = data_training.tail(100)
-test_data = past_100_days.append(data_testing, ignore_index=True)
+
+#####################################
+#           TESTING DATA            #
+#####################################
+test_data = pd.concat([data_training.tail(100), data_testing])
 testing_data_array = scaler.fit_transform(test_data)
 
-x_test = [] 
-y_test= []  
+x_test = []                          # feature data
+y_test = dataset[training_data_len:] # label data 
 
 for i in range(100, testing_data_array.shape[0]):
     x_test.append(testing_data_array[i-100: i])
-    y_test.append(testing_data_array[i, 0])
     
-x_test, y_test = numpy.array(x_test), numpy.array(y_test)
-x_train = numpy.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-# print(x_test.shape)
-# print(x_test)
-# print(y_test.shape)
+x_test = np.array(x_test)
+x_train = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
-# Predictions
+
+#####################################
+#           PREDICTIONS             #
+#####################################
 y_predicted = model.predict(x_test)
+y_predicted = scaler.inverse_transform(y_predicted)
 
-scaler = scaler.scale_
-scale_factor = 1 / scaler[0]
-y_predicted = y_predicted * scale_factor
-y_test = y_test * scale_factor
-
+# Get the root mean squared error (RMSE)
+rmse = np.sqrt(np.mean(y_predicted - y_test)**2)
+print('RMSE: ', rmse)
 
 # Predictions Graph
-st.subheader('Predicted vs Original Data')
-fig2 = plt.figure(figsize = (12,6))
+fig2 = plt.figure(figsize = (16,8))
+st.subheader('Complete Data Set')
+train = closingData[:training_data_len]
+test = closingData[training_data_len:]
+test['Predictions'] = y_predicted
+
+plt.xlabel('Time', fontsize=12)
+plt.ylabel('Price (USD)', fontsize=12)
+plt.plot(train['Close'], 'b')
+plt.plot(test[['Close','Predictions']])
+plt.legend(['Train', 'Test', 'Predictions'], loc='best')
+st.pyplot(fig2)
+
+fig3 = plt.figure(figsize = (16,8))
+st.subheader('Predicted vs Original (Test Data)')
+plt.xlabel('Time', fontsize=12)
+plt.ylabel('Price (USD)', fontsize=12)
 plt.plot(y_test, 'b', label="Original Price")
 plt.plot(y_predicted, 'c', label="Predicted Price")
-plt.xlabel('Time')
-plt.ylabel('Price (USD)')
-plt.legend()
-st.pyplot(fig2)
+plt.legend(loc='best')
+st.pyplot(fig3)
